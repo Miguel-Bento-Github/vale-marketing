9d416071d297df9b963cf5f3c55acc09f5d0afa2
b3nt0 <51338450+Miguel-Bento-Github@users.noreply.github.com>
2026-03-23 10:36:42 +0100

    feat: add Vue integration and D3 dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://valepark.org',
-  integrations: [sitemap()],
+  integrations: [sitemap(), vue()],
   vite: {
     plugins: [tailwindcss()],
   },
